0ee5dea2ac0ab0e67ff83d3a3d15c4af080d808b
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-06-04 18:57:10 -0300

    versão com layout rota hábitos finalizada com get

--- src/PagesComponets/HabitosPages/HabitosPage.jsx
+++ src/PagesComponets/HabitosPages/HabitosPage.jsx
@@ -1,11 +1,88 @@
-import { AddHabitos, AvisoHabitos, MenuBottom, NavBar, PageContainerHabitos } from "./styled";
+import { AddHabitos, AvisoHabitos, ConfigAdd, ListaHabitos, MenuBottom, NavBar, PageContainerHabitos, SCMapLi,  SCli, SCul } from "./styled";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 
 export default function HabitosPage(){
+    const arrSem = ['D','S','T','Q','Q','S','S'];
+    const [expHadicionar, setExpAdicionar] = useState(false);
+    const [nomeHabito, setNomeHabito] = useState("");
+    const [arrDiaSemana, setArrDiaSemana] = useState([]);
+    const [desabilitar, setDesabilitar] = useState(false)
+    const acesso = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTQ2MywiaWF0IjoxNjg1OTExNTM2fQ.8RggUqSwTkpCC-XY7NM-r-PjqmPDl9sqKvGpHWdArhk"
+    const URL_Post = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits"    
+    const RAW_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit"
+    const navigate = useNavigate();
+    const arrNumSemana = [0,1,2,3,4,5,6]
+    const [meusHabitos, setMeusHabitos] = useState([]);
+    const [aux, setAux] = useState([])
 
+    useEffect( () => {
+
+        axios.create({   
+            
+        })
+        const promisse = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits', {headers: {Authorization: `Bearer ${acesso}`}, 
+        baseURL: RAW_URL});
+
+        promisse.then((resposta) => {
+            console.log(resposta.data)
+            setMeusHabitos(resposta.data)
+            
+        })
+        promisse.catch((erro) =>{
+            console.log(erro.reponse.data.messege)
+        })
+    },[]);
+
+    
+    function adicionarHabito(){
+        setExpAdicionar(true);
+    }
+
+    function addSemana(dia, index){
+        let arr = [...arrDiaSemana,index]
+        setArrDiaSemana(arr)
+        }
+
+    function salvarHabito(e){
+            e.preventDefault()
+            console.log(nomeHabito)
+            console.log(arrDiaSemana)
+            setDesabilitar(true)
+
+
+            
+
+        const obj = 
+        {
+            name: nomeHabito,
+            days: arrDiaSemana
+        }
+    
+      
+        axios.create({   
+            headers: {Authorization: `Bearer ${acesso}`}, 
+            baseURL: RAW_URL, 
+        })
+        
+        .post(URL_Post, obj)
+        .then((res) => {
+            console.log(res.data)
+            setDesabilitar(false)
+            
+        })
+        .catch((err) => {
+            alert(err.reponse.data.messege)
+            setDesabilitar(false)
+
+        })
+
+    }
 
     return(
         <PageContainerHabitos>
@@ -17,15 +94,65 @@ export default function HabitosPage(){
 
             <AddHabitos>
                 <p>Meus hábitos</p>
-                <button><p>+</p></button>
+                <button onClick={adicionarHabito}><p>+</p></button>
             </AddHabitos>
 
-            <AvisoHabitos>
+            <ConfigAdd onSubmit={salvarHabito} 
+            
+            expHadicionar = {expHadicionar}>
+                <input disabled={desabilitar} placeholder="nome do hábito" onChange={(e) => setNomeHabito(e.target.value)}/>
+                
+                <SCul>
+                    {arrSem.map((dia, index )=>(
+
+                        <SCli disabled={desabilitar}
+                        key={index} 
+                        arrDiaSemana ={arrDiaSemana}
+                        index = {index}
+                    
+                        onClick={()=> addSemana(dia, index)}
+                        >
+                            {dia}
+                        </SCli>
+                    ))}
+                </SCul>
+
+                <span>
+                <p onClick={()=> setExpAdicionar(false)}>Cancelar</p>
+                <button disabled={desabilitar} type="submit" >Salvar</button>
+                </span>
+
+            </ConfigAdd>
+                        {meusHabitos.length == 0 &&
+            <AvisoHabitos >
             Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
             </AvisoHabitos>
+                        }
 
+                        {meusHabitos.length >0 &&
+            <ListaHabitos>
+                        {meusHabitos.map((habito, index) => (
+                        <li key={index}>
+                            <p>{habito.name}</p>
+                            <div>
+                                {arrSem.map((dia, i )=>(
+                                    <SCMapLi 
+                                    key={i}
+                                    meusHabitos = {habito.days}
+                                    index = {i}
+                                    diaSemana = {arrNumSemana}                                    
+                                    >
+                                        {dia}
+                                    </SCMapLi>
+                                ))}
+                            </div>
+                            <img src="/src/assets/dump.svg" alt="trash"/>
+                        </li>
+                        ))}
+            </ListaHabitos>
+                        }
             <MenuBottom>
-                <button>Hábitos</button>
+                <button onClick={()=> navigate('/habitos')}>Hábitos</button>
                 <button>Hoje</button>
                 <button>Histórico</button>
             </MenuBottom>
--- src/PagesComponets/HabitosPages/styled.js
+++ src/PagesComponets/HabitosPages/styled.js
@@ -10,6 +10,7 @@ export const NavBar = styled.div`
     height: 70px;
     background: #126BA5;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+    z-index: 1;
 
     img:nth-child(1){
         position: absolute;
@@ -31,7 +32,7 @@ export const NavBar = styled.div`
 `;
 
 export const AddHabitos = styled.div`
-    margin-top: 75px;
+    margin-top: 50px;
     width: 100%;
     height: 70px;
     display: flex;
@@ -67,7 +68,6 @@ export const AddHabitos = styled.div`
 `;
 
 export const AvisoHabitos = styled.p`
-
 font-family: 'Lexend Deca';
 font-style: normal;
 font-weight: 400;
@@ -77,11 +77,161 @@ color: #666666;
 
 `;
 
+
+export const ListaHabitos = styled.ul`
+        padding-bottom: 90px;
+        box-sizing: border-box;
+    li{
+        width: 340px;
+        height: 100px;
+        background: #FFFFFF;
+        border-radius: 5px;
+        padding: 13px;
+        box-sizing: border-box;
+        position: relative;
+        margin-bottom: 10px;
+        p{
+            font-style: normal;
+            font-weight: 400;
+            font-size: 19.976px;
+            line-height: 25px;
+            color: #666666;
+        }
+        
+        img{
+            position: absolute;
+            right: 11px;
+            top:11px;
+        }
+        div{
+            display: flex;
+           
+        }
+    }
+
+`
+
+
+export const SCMapLi = styled.div` 
+                width: 30px;
+                height: 30px;
+                display: flex;
+                justify-content: center;
+                margin-top: 10px;
+                
+                background: ${props => props.meusHabitos.includes(props.index)? "#CFCFCF" : "#fff"};
+                border: 1px solid #D5D5D5;
+                border-radius: 5px;
+                margin-right: 5px ;
+
+                font-weight: 500;
+                font-size: 19.976px;
+                line-height: 27px;
+                color:${props => props.meusHabitos.includes(props.index)? "#fff" : "#DBDBDB"}; ;
+                text-align: center;
+            `
+
+export const ConfigAdd = styled.form`
+    width: 340px;
+    height: 180px;
+    position: relative;
+
+    background: #FFFFFF;
+    border-radius: 5px;
+    margin-bottom: 29px;
+    display: ${props => props.expHadicionar==false ? "none" : "flex"};
+    flex-direction: column;
+    padding: 18px;
+    box-sizing: border-box;
+
+    span{
+        position: absolute;
+        right: 0;
+        bottom: 16px;
+        display: flex;
+        align-items: center;
+        margin-right: 15px;
+        p{
+           
+            font-style: normal;
+            font-weight: 400;
+            font-size: 15.976px;
+            line-height: 20px;
+
+            text-align: center;
+            color: #52B6FF;
+            margin-right: 23px;
+        }
+            button{
+                width: 84px;
+                height: 35px;
+                background: #52B6FF;
+                border-radius: 4.63636px;
+
+                font-style: normal;
+                font-weight: 400;
+                font-size: 15.976px;
+                line-height: 35px;
+                text-align: center;
+                color: #FFFFFF;
+                border: solid 1px #fff;
+            };
+        };
+    
+
+    input{
+        border: 1px solid #D4D4D4;
+        border-radius: 5px;
+        width: 100%;
+        height: 45px;
+        margin-bottom: 6px;
+        padding: 11px;
+        box-sizing: border-box;
+
+        ::placeholder{
+        font-weight: 400;
+        font-size: 19.976px;
+        line-height: 25px;
+        color: #DBDBDB;
+        }
+    };
+
+   
+`;
+export const SCul = styled.ul`
+
+    display: flex;
+     
+
+
+`;
+export const SCli = styled.li`
+            width: 30px;
+            height: 30px;
+            
+            background: ${props => props.arrDiaSemana.includes(props.index)? "#CFCFCF" : "#fff"};
+            border: 1px solid #D5D5D5;
+            border-radius: 5px;
+            margin-right: 5px ;
+
+            font-weight: 500;
+            font-size: 19.976px;
+            line-height: 25px;
+            color: ${props => props.arrDiaSemana.includes(props.index)? "#fff" : "#DBDBDB"};
+            text-align: center;
+            
+`;
+
 export const PageContainerHabitos = styled.div`
    width: 100%;
    height: 100%;
    background:#F2F2F2;
-   padding: 17px;
+   padding-top: 17px;
+   padding-right: 17px;
+   padding-left: 17px;
+   padding-bottom: 30px;
+  
+
    box-sizing: border-box;
    position: absolute;
    top:0;
